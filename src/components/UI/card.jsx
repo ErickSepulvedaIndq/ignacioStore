940a618691ae8d70b1c960a3dfe0332bff297d95
ErickSepulvedaIndq <erick.sepulveda@indqcorp.com>
2026-02-17 13:40:40 -0700

    logOut button created

--- src/components/UI/card.jsx
+++ src/components/UI/card.jsx
@@ -24,14 +24,12 @@ export default function Card({ productName, price, img, setCartCount}) {
                     </div>
                     <h2 className="font-bold pb-2 pl-2">{productName}</h2>
                     <p className="font-bold pb-2 pl-2">${price}</p>
-                    <div className="flex items-center justify-center gap-10">
+                    <div className="flex items-center justify-center gap-10 h-10">
                         <img src={Minus} alt="Plus" className="h-6 hover:rounded-full hover:scale-110 hover:bg-[#D8D7D5] cursor-pointer" onClick={() => handleQuantityChange(quantity - 1)} disabled={quantity === 1}/>
                         <p className="font-bold">{quantity}</p>
                         <img src={Plus} alt="Minus" className="h-6 hover:rounded-lg hover:scale-110 hover:bg-[#D8D7D5] cursor-pointer" onClick={() => handleQuantityChange(quantity + 1)}/>
                     </div>
-                    <div className="max-h-0 overflow-hidden
-                group-hover:max-h-40
-                transition-all duration-300 group-hover:pt-4">
+                    <div className="max-h-0 overflow-hidden group-hover:max-h-40 transition-all duration-300 group-hover:pt-4">
                         <div className="flex flex-col gap-2">
                         
                             <button className="w-full bg-[#3041A0] p-2 text-white rounded-lg cursor-pointer hover:bg-[#25327D]"
@@ -39,7 +37,7 @@ export default function Card({ productName, price, img, setCartCount}) {
                             Agregar al carrito
                             </button>
 
-                            <button className="w-full bg-[#FFCA1A] p-2 text-white rounded-lg cursor-pointer hover:bg-[#E5B816]">
+                            <button className="w-full bg-[#FFA41C] p-2 text-white rounded-lg cursor-pointer hover:bg-[#FF8F00]">
                             Comprar
                             </button>
                         </div>
--- src/components/UI/header.jsx
+++ src/components/UI/header.jsx
@@ -11,17 +11,19 @@ export default function Header({ cartCount}){
                 <input type="text" className="bg-white text-black p-2 rounded-lg w-full outline-none" placeholder="Buscar producto..." />
                 <img src={Lupa} alt="Lupa" className='h-9 hover:scale-110 transition-transform cursor-pointer'/>
             </div>
-            <div className="relative w-fit">
-                <img
-                    src={Carrito}
-                    alt="Carrito"
-                    className="h-10 hover:scale-110 transition-transform cursor-pointer"
-                />
+            <div className='flex items-center gap-4'>
+                <img src={LogOut} alt="LogOut" className='h-10 hover:scale-110 transition-transform cursor-pointer'/>
+                <div className="relative w-fit">
+                    <img
+                        src={Carrito}
+                        alt="Carrito"
+                        className="h-10 hover:scale-110 transition-transform cursor-pointer"
+                    />
 
-                <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs px-2 py-[2px] rounded-full" hidden={cartCount === 0}>
-                    {cartCount}
-                </span>
-                
+                    <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs px-2 py-[2px] rounded-full" hidden={cartCount === 0}>
+                        {cartCount}
+                    </span>
+                </div>
             </div>
         </header>
     )
