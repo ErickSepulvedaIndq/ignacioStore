b8afa17681fe6d68df3410ca87a2bc097c9bbdd5
KevinOmarM <kevin.montalvo@indqcorp.com>
2026-02-19 16:27:11 -0600

    feat: add confirm action

---
+++ src/components/UI/ConfirmAction.js
@@ -0,0 +1,36 @@
+import Swal from "sweetalert2";
+
+export const ConfirmAction = async ({
+    title,
+    text,
+    onConfirm,
+    confirmButtonText = "Confirmar",
+    cancelButtonText = "Cancelar",
+    icon = "question",
+}) => {
+    const result = await Swal.fire({
+        title,
+        text,
+        icon,
+        showCancelButton: true,
+        confirmButtonText,
+        cancelButtonText,
+        reverseButtons: true,
+    });
+
+    if (result.isConfirmed && onConfirm) {
+        try {
+            Swal.showLoading();
+            await onConfirm();
+            Swal.close();
+        } catch (err) {
+            console.error("Error en confirmAction:", err);
+            Swal.close();
+            Swal.fire({
+                icon: "error",
+                title: "Ocurrió un error",
+                text: err.message || "Intenta nuevamente",
+            });
+        }
+    }
+};
