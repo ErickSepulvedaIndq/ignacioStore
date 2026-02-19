75056ff445cc71acc2d0fb34e182deb0b5fc634e
KevinOmarM <kevin.montalvo@indqcorp.com>
2026-02-18 18:23:07 -0600

    feat: dynamic products visualization

--- src/pages/product.jsx
+++ src/pages/product.jsx
@@ -1,22 +1,36 @@
 import { useSearch } from '../context/SearchContext';
 import Product1 from '../assets/doritos-nachos.png';
 import Card from '../components/UI/card';
-import { useMemo } from 'react';
-
-// necesitamos traer la informacion de la API para poder plasmarla aqui
-// por mientras pues se llena automaticamente
-const productsTest = Array.from({ length: 10 }).map((_, i) => ({
-    id: `product-${i}`,
-    name: `Producto ${i + 1}`,
-    price: ((i * 13.2) % 100 + 20).toFixed(2),
-    img: Product1,
-}));
+import { useMemo, useState, useEffect } from 'react';
+import { getAllProducts } from '../services/productService';
+import Loading from '../components/UI/Loading';
 
 export default function Product() {
     // TODO: Fetch productos desde API
-    const productos = productsTest;
-    const { normalizedSearch } = useSearch();
+    const [productos, setProductos] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
+    const { normalizedSearch } = useSearch();
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                const response = await getAllProducts();
+
+                // Ajusta dependiendo de tu customResponse
+                // Si tu backend responde { data: [...] }
+                setProductos(response.data || response);
+
+            } catch (err) {
+                setError("Error al cargar productos");
+                console.error(err);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchProducts();
+    }, []);
     const filteredProducts = useMemo(() => {
         if (!normalizedSearch) {
             return productos;
@@ -27,20 +41,28 @@ export default function Product() {
         );
     }, [productos, normalizedSearch]);
 
+    if (loading) {
+        return <Loading/>
+    }
+
+    if (error) {
+        return <div className="p-10 text-center text-red-500">{error}</div>;
+    }
+
     return (
         <div className="container mx-auto p-6">
             <h1 className="text-3xl font-bold mb-6 text-gray-800">Productos</h1>
 
             <div className="flex flex-wrap items-center justify-center">
-                {filteredProducts.map((product) => (
-                    <Card
-                        key={product.id}
-                        productId={product.id}
-                        productName={product.name}
-                        price={product.price}
-                        img={product.img}
-                    />
-                ))}
+            {filteredProducts.map((product) => (
+                <Card
+                    key={product._id}
+                    productId={product._id}
+                    productName={product.name}
+                    price={product.price}
+                    img={product.image?.url}
+                />
+            ))}
 
                 {filteredProducts.length === 0 && (
                     <p className="text-gray-500 py-10">No se encontraron productos, verifique su texto de busqueda</p>
